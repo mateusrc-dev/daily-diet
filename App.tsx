e758936d394e3f4ad6ec88e23d81bc8afd16dae1
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-06 14:51:29 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { Diets } from './src/screens/Diets';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Diets />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Diets/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #FAFAFA;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text`
+  color: #1B1D1E;
+  font-size: 16px;
+  line-height: 1.3;
+`
